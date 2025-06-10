890716c712bcdf4e3ffbecb8980c817937371c26
Colin Palmer <11085027+colpalm@users.noreply.github.com>
2025-06-09 23:57:51 -0400

    Initial LLM Integration #2
    Added integration test skeleton for LLM client

---
+++ backend/tests/integration/test_llm_client_integration.py
@@ -0,0 +1,12 @@
+
+
+class TestLLMClientIntegration:
+    """Integration tests for LLM clients"""
+
+    def test_client_can_generate_a_response(self):
+        """
+        GIVEN I have an <llm client> with valid credentials
+        WHEN I send a simple prompt to the LLM API
+        THEN I should get a response back
+        """
+        pass
