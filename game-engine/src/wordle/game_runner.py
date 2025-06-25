13a442670cef42f23211f3f4956f6acde3c86070
Colin Palmer <11085027+colpalm@users.noreply.github.com>
2025-06-24 23:51:15 -0400

    #2 llm-game-runner - Inital GameRunner class

---
+++ game-engine/src/wordle/game_runner.py
@@ -0,0 +1,19 @@
+
+
+class GameRunner:
+    """
+    GameRunner that orchestrates a complete Wordle game session.
+    """
+
+    def __init__(self):
+        pass
+
+    def run_complete_game(self):
+        pass
+
+    def _initialize_game(self):
+        pass
+
+    def _play_game(self):
+        pass
+
